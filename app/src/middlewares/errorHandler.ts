b5d04f92f7e71c895a443f18d4efff93fff13e36
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-08-07 21:24:04 -0300

    feat: adiciona tratamento de erro para campos duplicados

--- app/src/middlewares/errorHandler.ts
+++ app/src/middlewares/errorHandler.ts
@@ -2,12 +2,14 @@ import mongoose from "mongoose";
 import { Request, Response, NextFunction } from "express";
 import { BadRequestError, BaseError } from "../errors/index.js";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function handler(error: Error, req: Request, res: Response, next: NextFunction) {
-	console.log(error.message);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+export default function handler(error: any, req: Request, res: Response, next: NextFunction) {
+	console.log(error);
 	if (error instanceof mongoose.Error.ValidationError) {
 		const errorMessage = Object.values(error.errors).map(e => e.message).join("; ");
 		new BadRequestError(errorMessage).sendResponse(res);
+	} else if (error.code === 11000) { // TODO - tratamento inadequado, porém não encontrei outra maneira
+		new BadRequestError(`Duplicate key for path: ${JSON.stringify(error.keyValue)}`).sendResponse(res);
 	} else {
 		new BaseError().sendResponse(res);
 	}
